11103c39e7ea2125bea17e271c891f6411e08011
woai3c <411020382@qq.com>
2021-01-21 23:33:40 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // base: '/introduction-to-front-end-engineering/',
+    title: '带你入门前端工程',
+    description: 'Just playing around',
+    head: [
+        ['meta', { name: 'referrer', content: 'no-referrer' }],
+    ]
+}
