1bbbb40b8cabbbef0218bf88a117692ea00a2dd6
woai3c <411020382@qq.com>
2021-01-22 11:01:58 +0800

    feat: 添加小书所有内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,29 @@
 module.exports = {
-    // base: '/introduction-to-front-end-engineering/',
+    base: '/introduction-to-front-end-engineering/',
     title: '带你入门前端工程',
-    description: 'Just playing around',
     head: [
         ['meta', { name: 'referrer', content: 'no-referrer' }],
-    ]
+    ],
+    themeConfig: {
+        sidebar: [
+            '/',
+            ['/01', '技术选型'],
+            ['/02', '统一规范'],
+            ['/03', '前端组件化'],
+            ['/04', '测试'],
+            ['/05', '构建工具'],
+            ['/06', '自动化部署'],
+            ['/07', '前端监控'],
+            ['/08', '性能优化（一）'],
+            ['/09', '性能优化（二）'],
+            ['/10', '重构'],
+            ['/11', '微服务'],
+            ['/12', 'Serverless'],
+            ['/13', '参考资料'],
+        ],
+        displayAllHeaders: true,
+        repo: 'https://github.com/woai3c/introduction-to-front-end-engineering',
+        repoLabel: '查看源码！',
+        smoothScroll: true,
+    }
 }
