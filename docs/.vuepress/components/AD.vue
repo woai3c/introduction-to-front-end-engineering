46b13b7cab9a5fe8405d486c9134e61b6c9c0f6b
woai3c <411020382@qq.com>
2022-12-28 18:45:26 +0800

    fix: fix ad error

--- docs/.vuepress/components/AD.vue
+++ docs/.vuepress/components/AD.vue
@@ -1,3 +1,14 @@
 <template>
     <div class="wwads-cn wwads-vertical wwads-sticky" data-id="204" style="max-width:180px"></div>
 </template>
+
+<script>
+const adScript = document.querySelector('#ad-script')
+if (!adScript) {
+    const script = document.createElement('script')
+    script.id = 'ad-script'
+    script.async = true
+    script.src = 'https://cdn.wwads.cn/js/makemoney.js'
+    document.head.appendChild(script)
+}
+</script>
